e0fff2d7c4b45bb1b13f65b95d78ad6b83841e63
bloodteller123 <chenzihan708@gmail.com>
2019-02-09 21:11:00 -0500

    Update search.js

--- src/search.js
+++ src/search.js
@@ -3,7 +3,35 @@ import React, {Component} from 'react';
 
 
 
-export function searchFunc(str, receipts){
+export function searchFunc(sArray, receipts){
+    let strArray = sArray;
+    let count=0;
     return receipts.filter((receipts)=>
-       receipts.ingredients.some(e=>e.indexOf(str)>=0))
-}
+     {
+     receipts.ingredients.forEach((e,index,arr)=>
+       {
+           console.log(e);
+             for(const ele of strArray){
+               console.log(strArray);
+            if(!e.includes(ele)){
+                console.log('---------');
+                   count=0;
+               }
+               else{
+                    count++;
+                    strArray=strArray.filter(e=>e!=ele);
+                    console.log(count);
+                    break;
+               }
+             }
+          console.log(count);
+          if(count!=sArray.length && !arr[index + 1])
+                count=0;
+           console.log(count==sArray.length);
+           console.log(sArray.length);
+       })
+      return count==sArray.length;
+     }  
+        );
+        
+    }
